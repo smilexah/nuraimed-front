68e0da68adad8622c8eb692315c7adecc7e92725
Meiirzhan Muratbekuly <smilexah@inbox.ru>
2025-08-27 19:23:27 +0500

    refactor axios instance; remove timeout setting for improved request handling

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: "https://api.di-clinic.kz/api", // process.env.NEXT_PUBLIC_API_BASE_URL ||
-    timeout: 10000,
+    // timeout: 10000,
     headers: {
         "Content-Type": "application/json",
     },
